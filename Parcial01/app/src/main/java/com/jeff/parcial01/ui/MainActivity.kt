d384bec6634f3f787c205b449235dfd2b273fd75
JeffreVaquerano <00066521@uca.edu.sv>
2023-05-22 15:27:05 -0600

    🎨Add Vistas

--- Parcial01/app/src/main/java/com/jeff/parcial01/ui/MainActivity.kt
+++ Parcial01/app/src/main/java/com/jeff/parcial01/ui/MainActivity.kt
@@ -3,10 +3,17 @@ package com.jeff.parcial01.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jeff.parcial01.R
+import com.jeff.parcial01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
