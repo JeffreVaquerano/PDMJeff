0ca283d1ba75708e700b205d06bc48a290a69f08
JeffreVaquerano <00066521@uca.edu.sv>
2023-05-22 14:08:02 -0600

    🚧 Add Capa Repository

---
+++ Parcial01/app/src/main/java/com/jeff/parcial01/repositories/MontanaRepository.kt
@@ -0,0 +1,11 @@
+package com.jeff.parcial01.repositories
+
+import com.jeff.parcial01.Montana
+import com.jeff.parcial01.data.model.montanaModel
+
+//Agregando Capa Repository
+class MontanaRepository(private val montanas: MutableList<montanaModel>) {
+
+    fun getMontanas() = montanas //Fun para obtener obj
+    fun addMontanas(montana: montanaModel) = montanas.add(montana) //Fun para agregar obj
+}
